e6d5e1f8dff4a9728ece03481ec480e837aebfb7
Carl Vander <carl.vander@matsmart.se>
2024-12-07 22:26:51 +0100

    Day 2

--- solutions/day2.py
+++ solutions/day2.py
@@ -1,5 +1,41 @@
-from solutions.file_utils import InputType
+from solutions.file_utils import InputType, read_input
 
 
-def solve_a(input_type: InputType):
-    return 0
+def _read_levels_matrix(input_type: InputType) -> list[list[int]]:
+    levels = read_input(input_type=input_type, day="2a")
+    return [[int(nr) for nr in level.split(" ")] for level in levels]
+
+
+def _is_safe(level: list[int]) -> bool:
+    diffs = [a - b for a, b in zip(level, level[1:])]
+
+    if not (all([diff > 0 for diff in diffs]) or all([diff < 0 for diff in diffs])):
+        return False
+
+    if any([abs(diff) > 3 for diff in diffs]):
+        return False
+
+    return True
+
+
+def _is_really_safe(level: list[int]) -> bool:
+    if _is_safe(level=level):
+        return True
+
+    for index in range(len(level)):
+        if _is_safe(level=level[:index] + level[index + 1 :]):
+            return True
+
+    return False
+
+
+def solve_a(input_type: InputType) -> int:
+    levels = _read_levels_matrix(input_type=input_type)
+    safe = [level for level in levels if _is_safe(level)]
+    return len(safe)
+
+
+def solve_b(input_type: InputType) -> int:
+    levels = _read_levels_matrix(input_type=input_type)
+    safe = [level for level in levels if _is_really_safe(level)]
+    return len(safe)
--- tests/test_day2.py
+++ tests/test_day2.py
@@ -1,10 +1,36 @@
 import pytest
 from solutions.file_utils import InputType
-from solutions.day2 import solve_a
+from solutions.day2 import solve_a, solve_b, _is_really_safe
+
 
 @pytest.mark.parametrize(
-    "input_type, expected", [(InputType.SAMPLE, 0), (InputType.FULL, 0)]
+    "input_type, expected", [(InputType.SAMPLE, 2), (InputType.FULL, 663)]
 )
 def test_solve_a(input_type: InputType, expected: int):
     actual = solve_a(input_type=input_type)
+    assert actual == expected
+
+
+# 736 is too high!
+@pytest.mark.parametrize(
+    "input_type, expected", [(InputType.SAMPLE, 4), (InputType.FULL, 692)]
+)
+def test_solve_b(input_type: InputType, expected: int):
+    actual = solve_b(input_type=input_type)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "level,expected",
+    [
+        ([1, 2, 3, 4], True),
+        ([4, 3, 2, 1], True),
+        ([7, 3, 2, 1], True),
+        ([10, 6, 2, 1], False),
+        ([10, 6, 2, 4], False),
+        ([10, 11, 8, 5], True),
+    ],
+)
+def test_is_really_safe(level: list[int], expected: bool):
+    actual = _is_really_safe(level=level)
     assert actual == expected
